336a1bd1612a4f3958ccc4513c36306073be3396
FranciscoDitutala <ditutalafrancisco99@gmail.com>
2025-05-19 23:36:00 +0100

    Product

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -2,12 +2,15 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
@@ -15,32 +18,45 @@ public class Category implements Serializable {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;	
-	
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
+
+	//@ManyToMany(mappedBy = "categories")
+	@Transient
+	private Set<Product> products;
+
 	public Category() {
-		
+
 	}
+
 	public Category(Long id, String name) {
 		super();
 		this.id = id;
 		this.name = name;
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
 	}
 	@Override
 	public int hashCode() {
